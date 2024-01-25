552022043eaaaacf4ba749afc889fcc38cb07c1c
Andreea Brîndea <andreeabrindea61@gmail.com>
2024-01-26 00:55:11 +0200

    feat: add cors

--- index.py
+++ index.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
+
 from api.CatApi import cat_api
 from api.DogApi import dog_api
 
 app = Flask(__name__)
 app.register_blueprint(cat_api)
 app.register_blueprint(dog_api)
+CORS(app)
